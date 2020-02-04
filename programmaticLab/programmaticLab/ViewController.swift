0cd0dc2f46b92fd917637d6822e6190c5bfb31a1
Ahad Islam <ahad.islam624@gmail.com>
2020-02-04 11:52:46 -0500

    deleted main interface programmatic scene delegate added color guessing model

---
+++ programmaticLab/programmaticLab/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  programmaticLab
+//
+//  Created by Ahad Islam on 2/4/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
