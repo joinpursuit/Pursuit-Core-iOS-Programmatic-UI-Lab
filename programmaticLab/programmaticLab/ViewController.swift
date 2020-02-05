2b645e4ef6831ab95f575eb042aefc8aa61ca87f
Ahad Islam <ahad.islam624@gmail.com>
2020-02-04 20:02:37 -0500

    color guesses correctly, added targets to all the buttons. created a stack view and a scrollable to keep track created a reset button to make the score reset and all the buttons change
    created a func that when the button's pressed it should work
    setup all the views programatically.

--- programmaticLab/programmaticLab/ViewController.swift
+++ programmaticLab/programmaticLab/ViewController.swift
@@ -13,19 +13,37 @@ class ViewController: UIViewController {
     private lazy var redButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .red
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         return button
     }()
     
     private lazy var greenButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .green
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         return button
     }()
     
     private lazy var blueButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [redButton, greenButton, blueButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private lazy var scoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Score: \(score)"
+        label.textAlignment = .center
+        return label
     }()
     
     private lazy var colorView: UIView = {
@@ -34,10 +52,25 @@ class ViewController: UIViewController {
         return view
     }()
     
-    var guessColor = ColorGuessingModel()
+    private lazy var resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset Game", for: .normal)
+        button.addTarget(self, action: #selector(reset(_:)), for: .touchUpInside)
+        button.isHidden = true
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    private let guessColor = ColorGuessingModel()
+    
+    private var score = 0 {
+        didSet {
+            labelChange()
+        }
+    }
     
     // MARK: Override Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
@@ -46,10 +79,34 @@ class ViewController: UIViewController {
     
     // MARK: Private Methods
     
+    @objc private func buttonPressed(_ sender: UIButton) {
+        if guessColor.isDominant(guess: sender.backgroundColor ?? .black) {
+            colorView.backgroundColor = guessColor.getNewColor()
+            score += 1
+        } else {
+            resetButton.isHidden = false
+            [redButton, greenButton, blueButton].forEach { $0.isEnabled = false }
+            scoreLabel.text = "You Lost!"
+        }
+    }
+    
+    @objc private func reset(_ sender: UIButton) {
+        score = 0
+        colorView.backgroundColor = guessColor.getNewColor()
+        [redButton, greenButton, blueButton].forEach { $0.isEnabled = true }
+        sender.isHidden = true
+    }
+    
+    private func labelChange() {
+        scoreLabel.text = "Score: \(score)"
+    }
+    
     private func configureView() {
         view.backgroundColor = .systemOrange
         setupColorView()
         setupButtons()
+        setupLabel()
+        setupResetButton()
     }
     
     private func setupColorView() {
@@ -64,12 +121,6 @@ class ViewController: UIViewController {
     }
     
     private func setupButtons() {
-        
-        let stackView = UIStackView(arrangedSubviews: [redButton, greenButton, blueButton])
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.spacing = 20
-        
         view.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -77,10 +128,26 @@ class ViewController: UIViewController {
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.125)])
-        
-        
     }
-
-
+    
+    private func setupLabel() {
+        view.addSubview(scoreLabel)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20),
+            scoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+    }
+    
+    private func setupResetButton() {
+        view.addSubview(resetButton)
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            resetButton.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 20),
+            resetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            resetButton.widthAnchor.constraint(equalToConstant: view.frame.width / 4)])
+    }
+    
+    
+    
 }
 
